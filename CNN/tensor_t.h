eb5796c05422641558031b787103447bfa282bd0
VirosaLi <fangchen.li@outlook.com>
2019-08-13 00:29:35 -0500

    "emplace push"

--- CNN/tensor_t.h
+++ CNN/tensor_t.h
@@ -1,118 +1,141 @@
 #pragma once
+
 #include "point_t.h"
 #include <cassert>
 #include <vector>
-#include <string.h>
+#include <cstring>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <iterator>
 
 template<typename T>
-struct tensor_t
-{
-	T * data;
+struct tensor_t {
+    T *data;
 
-	tdsize size;
+    tdsize size{};
 
-	tensor_t( int _x, int _y, int _z )
-	{
-		data = new T[_x * _y * _z];
-		size.x = _x;
-		size.y = _y;
-		size.z = _z;
-	}
+    tensor_t(int _x, int _y, int _z) {
+        data = new T[_x * _y * _z];
+        size.x = _x;
+        size.y = _y;
+        size.z = _z;
+    }
 
-	tensor_t( const tensor_t& other )
-	{
-		data = new T[other.size.x *other.size.y *other.size.z];
-		memcpy(
-			this->data,
-			other.data,
-			other.size.x *other.size.y *other.size.z * sizeof( T )
-		);
-		this->size = other.size;
-	}
+    tensor_t(const tensor_t &other) {
+        data = new T[other.size.x * other.size.y * other.size.z];
+        memcpy(
+                this->data,
+                other.data,
+                other.size.x * other.size.y * other.size.z * sizeof(T)
+        );
+        this->size = other.size;
+    }
 
-	tensor_t<T> operator+( tensor_t<T>& other )
-	{
-		tensor_t<T> clone( *this );
-		for ( int i = 0; i < other.size.x * other.size.y * other.size.z; i++ )
-			clone.data[i] += other.data[i];
-		return clone;
-	}
+    tensor_t<T> operator+(tensor_t<T> &other) {
+        tensor_t<T> clone(*this);
+        for (int i = 0; i < other.size.x * other.size.y * other.size.z; i++)
+            clone.data[i] += other.data[i];
+        return clone;
+    }
 
-	tensor_t<T> operator-( tensor_t<T>& other )
-	{
-		tensor_t<T> clone( *this );
-		for ( int i = 0; i < other.size.x * other.size.y * other.size.z; i++ )
-			clone.data[i] -= other.data[i];
-		return clone;
-	}
+    tensor_t<T> operator-(tensor_t<T> &other) {
+        tensor_t<T> clone(*this);
+        for (int i = 0; i < other.size.x * other.size.y * other.size.z; i++)
+            clone.data[i] -= other.data[i];
+        return clone;
+    }
 
-	T& operator()( int _x, int _y, int _z )
-	{
-		return this->get( _x, _y, _z );
-	}
+    T &operator()(int _x, int _y, int _z) {
+        return this->get(_x, _y, _z);
+    }
 
-	T& get( int _x, int _y, int _z )
-	{
-		assert( _x >= 0 && _y >= 0 && _z >= 0 );
-		assert( _x < size.x && _y < size.y && _z < size.z );
+    T &get(int _x, int _y, int _z) {
+        assert(_x >= 0 && _y >= 0 && _z >= 0);
+        assert(_x < size.x && _y < size.y && _z < size.z);
 
-		return data[
-			_z * (size.x * size.y) +
-				_y * (size.x) +
-				_x
-		];
-	}
+        return data[
+                _z * (size.x * size.y) +
+                _y * (size.x) +
+                _x
+        ];
+    }
 
-	void copy_from( std::vector<std::vector<std::vector<T>>> data )
-	{
-		int z = data.size();
-		int y = data[0].size();
-		int x = data[0][0].size();
+    void copy_from(std::vector<std::vector<std::vector<T>>> vector) {
+        int z = vector.size();
+        int y = vector[0].size();
+        int x = vector[0][0].size();
 
-		for ( int i = 0; i < x; i++ )
-			for ( int j = 0; j < y; j++ )
-				for ( int k = 0; k < z; k++ )
-					get( i, j, k ) = data[k][j][i];
-	}
+        for (int i = 0; i < x; i++)
+            for (int j = 0; j < y; j++)
+                for (int k = 0; k < z; k++)
+                    get(i, j, k) = vector[k][j][i];
+    }
 
-	~tensor_t()
-	{
-		delete[] data;
-	}
+    ~tensor_t() {
+        delete[] data;
+    }
 };
 
-static void print_tensor( tensor_t<float>& data )
-{
-	int mx = data.size.x;
-	int my = data.size.y;
-	int mz = data.size.z;
+static void print_tensor(tensor_t<float> &data) {
+    int mx = data.size.x;
+    int my = data.size.y;
+    int mz = data.size.z;
 
-	for ( int z = 0; z < mz; z++ )
-	{
-		printf( "[Dim%d]\n", z );
-		for ( int y = 0; y < my; y++ )
-		{
-			for ( int x = 0; x < mx; x++ )
-			{
-				printf( "%.2f \t", (float)data.get( x, y, z ) );
-			}
-			printf( "\n" );
-		}
-	}
+    for (int z = 0; z < mz; z++) {
+        printf("[Dim%d]\n", z);
+        for (int y = 0; y < my; y++) {
+            for (int x = 0; x < mx; x++) {
+                printf("%.2f \t", (float) data.get(x, y, z));
+            }
+            printf("\n");
+        }
+    }
 }
 
-static tensor_t<float> to_tensor( std::vector<std::vector<std::vector<float>>> data )
-{
-	int z = data.size();
-	int y = data[0].size();
-	int x = data[0][0].size();
+static tensor_t<float> to_tensor(std::vector<std::vector<std::vector<float>>> data) {
+    int z = data.size();
+    int y = data[0].size();
+    int x = data[0][0].size();
 
 
-	tensor_t<float> t( x, y, z );
+    tensor_t<float> t(x, y, z);
 
-	for ( int i = 0; i < x; i++ )
-		for ( int j = 0; j < y; j++ )
-			for ( int k = 0; k < z; k++ )
-				t( i, j, k ) = data[k][j][i];
-	return t;
+    for (int i = 0; i < x; i++)
+        for (int j = 0; j < y; j++)
+            for (int k = 0; k < z; k++)
+                t(i, j, k) = data[k][j][i];
+    return t;
+}
+
+static std::string tensor_to_string(tensor_t<float> &data) {
+    std::stringstream ss;
+    ss << data.size.x << " " << data.size.y << " " << data.size.z << " ";
+
+    for (int z = 0; z < data.size.z; ++z) {
+        for (int y = 0; y < data.size.y; ++y) {
+            for (int x = 0; x < data.size.x; ++x) {
+                ss << data.get(x, y, z) << " ";
+            }
+        }
+    }
+    return ss.str();
+}
+
+static tensor_t<float> string_to_tensor(const std::string& s) {
+    std::istringstream iss(s);
+    std::vector<float> v((std::istream_iterator<float>(iss)), std::istream_iterator<float>());
+    int x = (int) v[0];
+    int y = (int) v[1];
+    int z = (int) v[2];
+
+    tensor_t<float> t(x, y, z);
+    for (int i = 0; i < z; ++i) {
+        for (int j = 0; j < y; ++j) {
+            for (int k = 0; k < x; ++k) {
+                t(k, j, i) = v[ x*y*i + y*j + k + 3];
+            }
+        }
+    }
+    return t;
 }
--- Example MNIST/example2.cpp
+++ Example MNIST/example2.cpp
@@ -8,210 +8,190 @@
 
 using namespace std;
 
-float train( vector<layer_t*>& layers, tensor_t<float>& data, tensor_t<float>& expected )
-{
-	for ( int i = 0; i < layers.size(); i++ )
-	{
-		if ( i == 0 )
-			activate( layers[i], data );
-		else
-			activate( layers[i], layers[i - 1]->out );
-	}
+float train(vector<layer_t *> &layers, tensor_t<float> &data, tensor_t<float> &expected) {
+    for (int i = 0; i < layers.size(); i++) {
+        if (i == 0)
+            activate(layers[i], data);
+        else
+            activate(layers[i], layers[i - 1]->out);
+    }
 
-	tensor_t<float> grads = layers.back()->out - expected;
+    tensor_t<float> grads = layers.back()->out - expected;
 
-	for ( int i = layers.size() - 1; i >= 0; i-- )
-	{
-		if ( i == layers.size() - 1 )
-			calc_grads( layers[i], grads );
-		else
-			calc_grads( layers[i], layers[i + 1]->grads_in );
-	}
+    for (int i = layers.size() - 1; i >= 0; i--) {
+        if (i == layers.size() - 1)
+            calc_grads(layers[i], grads);
+        else
+            calc_grads(layers[i], layers[i + 1]->grads_in);
+    }
 
-	for (auto & layer : layers)
-	{
-		fix_weights( layer );
-	}
+    for (auto &layer : layers) {
+        fix_weights(layer);
+    }
 
-	float err = 0;
-	for ( int i = 0; i < grads.size.x * grads.size.y * grads.size.z; i++ )
-	{
-		float f = expected.data[i];
-		if ( f > 0.5 )
-			err += abs(grads.data[i]);
-	}
-	return err * 100;
+    float err = 0;
+    for (int i = 0; i < grads.size.x * grads.size.y * grads.size.z; i++) {
+        float f = expected.data[i];
+        if (f > 0.5)
+            err += abs(grads.data[i]);
+    }
+    return err * 100;
 }
 
 
-void forward( vector<layer_t*>& layers, tensor_t<float>& data )
-{
-	for ( int i = 0; i < layers.size(); i++ )
-	{
-		if ( i == 0 )
-			activate( layers[i], data );
-		else
-			activate( layers[i], layers[i - 1]->out );
-	}
+void forward(vector<layer_t *> &layers, tensor_t<float> &data) {
+    for (int i = 0; i < layers.size(); i++) {
+        if (i == 0)
+            activate(layers[i], data);
+        else
+            activate(layers[i], layers[i - 1]->out);
+    }
 }
 
-struct case_t
-{
-	tensor_t<float> data;
-	tensor_t<float> out;
+struct case_t {
+    tensor_t<float> data;
+    tensor_t<float> out;
 };
 
-uint8_t* read_file( const char* szFile )
-{
-	ifstream file( szFile, ios::binary | ios::ate );
-	streamsize size = file.tellg();
-	file.seekg( 0, ios::beg );
+uint8_t *read_file(const char *szFile) {
+    ifstream file(szFile, ios::binary | ios::ate);
+    streamsize size = file.tellg();
+    file.seekg(0, ios::beg);
 
-	if ( size == -1 )
-		return nullptr;
+    if (size == -1)
+        return nullptr;
 
-	auto* buffer = new uint8_t[size];
-	file.read( (char*)buffer, size );
-	return buffer;
+    auto *buffer = new uint8_t[size];
+    file.read((char *) buffer, size);
+    return buffer;
 }
 
-vector<case_t> read_test_cases()
-{
-	vector<case_t> cases;
+vector<case_t> read_test_cases() {
+    vector<case_t> cases;
 
-	uint8_t* train_image = read_file( "train-images.idx3-ubyte" );
-	uint8_t* train_labels = read_file( "train-labels.idx1-ubyte" );
+    uint8_t *train_image = read_file("train-images.idx3-ubyte");
+    uint8_t *train_labels = read_file("train-labels.idx1-ubyte");
 
-	uint32_t case_count = byteswap_uint32( *(uint32_t*)(train_image + 4) );
+    uint32_t case_count = byteswap_uint32(*(uint32_t *) (train_image + 4));
 
-	for ( int i = 0; i < case_count; i++ )
-	{
-		case_t c
-		{
-			tensor_t<float>( 28, 28, 1 ),
-				tensor_t<float>( 10, 1, 1 )
-				};
+    for (int i = 0; i < case_count; i++) {
+        case_t c
+                {
+                        tensor_t<float>(28, 28, 1),
+                        tensor_t<float>(10, 1, 1)
+                };
 
-		uint8_t* img = train_image + 16 + i * (28 * 28);
-		uint8_t* label = train_labels + 8 + i;
+        uint8_t *img = train_image + 16 + i * (28 * 28);
+        uint8_t *label = train_labels + 8 + i;
 
-		for ( int x = 0; x < 28; x++ )
-			for ( int y = 0; y < 28; y++ )
-				c.data( x, y, 0 ) = img[x + y * 28] / 255.f;
+        for (int x = 0; x < 28; x++)
+            for (int y = 0; y < 28; y++)
+                c.data(x, y, 0) = img[x + y * 28] / 255.f;
 
-		for ( int b = 0; b < 10; b++ )
-			c.out( b, 0, 0 ) = *label == b ? 1.0f : 0.0f;
+        for (int b = 0; b < 10; b++)
+            c.out(b, 0, 0) = *label == b ? 1.0f : 0.0f;
 
-		cases.push_back( c );
-	}
-	delete[] train_image;
-	delete[] train_labels;
+        cases.push_back(c);
+    }
+    delete[] train_image;
+    delete[] train_labels;
 
-	return cases;
+    return cases;
 }
 
-int main()
-{
+int main() {
 
-	vector<case_t> cases = read_test_cases();
+    vector<case_t> cases = read_test_cases();
 
-	vector<layer_t*> layers;
+    vector<layer_t *> layers;
 
-	auto * layer1 = new conv_layer_t( 1, 5, 8, cases[0].data.size );		// 28 * 28 * 1 -> 24 * 24 * 8
-	auto * layer2 = new relu_layer_t( layer1->out.size );
-	auto * layer3 = new pool_layer_t( 2, 2, layer2->out.size );				// 24 * 24 * 8 -> 12 * 12 * 8
+//	auto * layer1 = new conv_layer_t( 1, 5, 8, cases[0].data.size );
+//	auto * layer2 = new relu_layer_t( layer1->out.size );
+//	auto * layer3 = new pool_layer_t( 2, 2, layer2->out.size );
+//
+//	auto * layer4 = new conv_layer_t( 1, 3, 10, layer3->out.size );
+//	auto * layer5 = new relu_layer_t( layer4->out.size );
+//	auto * layer6 = new pool_layer_t( 2, 2, layer5->out.size );
+//
+//	auto * layer7 = new fc_layer_t( layer6->out.size, 10 );
 
-	auto * layer4 = new conv_layer_t( 1, 3, 10, layer3->out.size );			// 12 * 12 * 6 -> 10 * 10 * 10
-	auto * layer5 = new relu_layer_t( layer4->out.size );
-	auto * layer6 = new pool_layer_t( 2, 2, layer5->out.size );				// 10 * 10 * 10 -> 5 * 5 * 10
-
-	auto * layer7 = new fc_layer_t( layer6->out.size, 10 );					// 4 * 4 * 16 -> 10
-
-	layers.push_back( (layer_t*)layer1 );
-	layers.push_back( (layer_t*)layer2 );
-	layers.push_back( (layer_t*)layer3 );
-	layers.push_back( (layer_t*)layer4 );
-	layers.push_back( (layer_t*)layer5 );
-	layers.push_back( (layer_t*)layer6 );
-	layers.push_back( (layer_t*)layer7 );
-
-
-
-	float amse = 0;
-	int ic = 0;
-
-	for ( long ep = 0; ep < 100000; )
-	{
-
-		for ( case_t& t : cases )
-		{
-			float xerr = train( layers, t.data, t.out );
-			amse += xerr;
-
-			ep++;
-			ic++;
-
-			if ( ep % 1000 == 0 )
-				cout << "case " << ep << " err=" << amse/ic << endl;
-
-			// if ( GetAsyncKeyState( VK_F1 ) & 0x8000 )
-			// {
-			//     printf( "err=%.4f%\n", amse / ic  );
-			//     goto end;
-			// }
-		}
-	}
-	// end:
+    layers.emplace_back((layer_t *) new conv_layer_t(1, 5, 8, cases[0].data.size)); // 28 * 28 * 1 -> 24 * 24 * 8
+    layers.emplace_back((layer_t *) new relu_layer_t(layers.back()->out.size));
+    layers.emplace_back((layer_t *) new pool_layer_t(2, 2, layers.back()->out.size)); // 24 * 24 * 8 -> 12 * 12 * 8
+    layers.emplace_back((layer_t *) new conv_layer_t(1, 3, 10, layers.back()->out.size)); // 12 * 12 * 6 -> 10 * 10 * 10
+    layers.emplace_back((layer_t *) new relu_layer_t(layers.back()->out.size));
+    layers.emplace_back((layer_t *) new pool_layer_t(2, 2, layers.back()->out.size)); // 10 * 10 * 10 -> 5 * 5 * 10
+    layers.emplace_back((layer_t *) new fc_layer_t(layers.back()->out.size, 10)); // 4 * 4 * 16 -> 10
 
 
 
-	while ( true )
-	{
-		uint8_t * data = read_file( "test.ppm" );
+    float amse = 0;
+    int ic = 0;
 
-		if ( data )
-		{
-			uint8_t * usable = data;
+    for (long ep = 0; ep < 100000;) {
 
-			while ( *(uint32_t*)usable != 0x0A353532 )
-				usable++;
+        for (case_t &t : cases) {
+            float xerr = train(layers, t.data, t.out);
+            amse += xerr;
+
+            ep++;
+            ic++;
+
+            if (ep % 1000 == 0)
+                cout << "case " << ep << " err=" << amse / ic << endl;
+
+            // if ( GetAsyncKeyState( VK_F1 ) & 0x8000 )
+            // {
+            //     printf( "err=%.4f%\n", amse / ic  );
+            //     goto end;
+            // }
+        }
+    }
+    // end:
+
+
+
+    while (true) {
+        uint8_t *data = read_file("test.ppm");
+
+        if (data) {
+            uint8_t *usable = data;
+
+            while (*(uint32_t *) usable != 0x0A353532)
+                usable++;
 
 #pragma pack(push, 1)
-			struct RGB
-			{
-				uint8_t r, g, b;
-			};
+            struct RGB {
+                uint8_t r, g, b;
+            };
 #pragma pack(pop)
 
-			RGB * rgb = (RGB*)usable;
+            RGB *rgb = (RGB *) usable;
 
-			tensor_t<float> image(28, 28, 1);
-			for ( int i = 0; i < 28; i++ )
-			{
-				for ( int j = 0; j < 28; j++ )
-				{
-					RGB rgb_ij = rgb[i * 28 + j];
-					image( j, i, 0 ) = ((((float)rgb_ij.r
-							      + rgb_ij.g
-							      + rgb_ij.b)
-							     / (3.0f*255.f)));
-				}
-			}
+            tensor_t<float> image(28, 28, 1);
+            for (int i = 0; i < 28; i++) {
+                for (int j = 0; j < 28; j++) {
+                    RGB rgb_ij = rgb[i * 28 + j];
+                    image(j, i, 0) = ((((float) rgb_ij.r
+                                        + rgb_ij.g
+                                        + rgb_ij.b)
+                                       / (3.0f * 255.f)));
+                }
+            }
 
-			forward( layers, image );
-			tensor_t<float>& out = layers.back()->out;
-			for ( int i = 0; i < 10; i++ )
-			{
-				printf( "[%i] %f\n", i, out( i, 0, 0 )*100.0f );
-			}
+            forward(layers, image);
+            tensor_t<float> &out = layers.back()->out;
+            for (int i = 0; i < 10; i++) {
+                printf("[%i] %f\n", i, out(i, 0, 0) * 100.0f);
+            }
 
-			delete[] data;
-		}
+            delete[] data;
+        }
 
-		struct timespec wait;
-		wait.tv_sec = 1;
-		wait.tv_nsec = 0;
-		nanosleep(&wait, nullptr);
-	}
-	return 0;
+        struct timespec wait;
+        wait.tv_sec = 1;
+        wait.tv_nsec = 0;
+        nanosleep(&wait, nullptr);
+    }
+    return 0;
 }
